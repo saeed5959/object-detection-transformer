a04ef134b2bec9392c63fe1ddd167c9d578f10bd
saeed <saeedmr881@gmail.com>
2023-06-05 00:42:13 +0330

    change poa

--- object_detection/head.py
+++ object_detection/head.py
@@ -11,6 +11,7 @@ class HeadDetect(nn.Module):
         self.dim = model_config.dim
         self.patch_num = model_config.patch_num
         self.class_num = model_config.class_num
+        self.poa_epoch = model_config.poa_epoch
         self.linear_similarity = nn.Linear(self.dim,self.dim)
         self.linear_class_1 = nn.Linear(self.dim + self.dim + self.patch_num, 256)
         self.linear_class_2 = nn.Linear(256, 1 + self.class_num + 4) 
@@ -19,11 +20,11 @@ class HeadDetect(nn.Module):
         x_linear = self.linear_similarity(x)
         similarity_matrix = sigmoid(torch.matmul(x_linear, x_linear.transpose(1,2)) / self.patch_num)
         
-        if epoch > model_config.poa_epoch:
+        if epoch > self.poa_epoch:
             similarity_matrix_main = similarity_matrix
         else:
             similarity_matrix_main = poa
-            
+
         b, m, n = similarity_matrix_main.size()
         
         average_patch_select = torch.matmul(similarity_matrix_main, x) / torch.sum(similarity_matrix_main, dim=-1).reshape(b,m,1).repeat(1,1,self.dim)
