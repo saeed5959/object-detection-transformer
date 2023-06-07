00a05ab5f30b3beef1c5e64b1cdb9753b1162f4f
saeed <saeedmr881@gmail.com>
2023-06-07 17:42:01 +0330

    inference

--- inference_dataset.py
+++ inference_dataset.py
@@ -47,6 +47,7 @@ def inference_img(img_path, model, img_out_path):
 
     #prepare input image
     img = img_preprocess_inference(img_path)
+    print(device)
     img.to(device)
     print(img.get_device())
     poa = []
